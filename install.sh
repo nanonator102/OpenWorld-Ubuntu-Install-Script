04ed39598c9805154f029661801c8d78c5dd4fad
Zachary Moroney <thenanonator102@gmail.com>
2022-11-25 08:37:44 +1100

    Updated to mod version 1.4.2

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ mkdir ./OpenWorld
 chmod +x run.sh
 mv run.sh ./OpenWorld/
 cd OpenWorld
-wget https://github.com/TastyLollipop/OpenWorld/releases/download/1.4.0/LinuxX64.zip
+wget https://github.com/TastyLollipop/OpenWorld/releases/download/1.4.2/LinuxX64.zip
 unzip LinuxX64.zip
 chmod +x Open\ World\ Server
 rm LinuxX64.zip
